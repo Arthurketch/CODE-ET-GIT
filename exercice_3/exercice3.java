e90dac50802f7c281d3c1ed19fa4e18845d0748c
Arthurketch <ketchanr@3il.fr>
2024-11-26 16:47:04 +0100

    Gestion de stock

--- exercice_3/exercice3.java
+++ exercice_3/exercice3.java
@@ -1,20 +1,39 @@
 public class StockManager {
 
-    // gere le stock
+	/**
+	 * Ajouter un stock
+	 * @param produit, stock
+	 */
+	public void ajoutStock(String produit, int stock) {
+		stock += quantite;
+        System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
+	}
+	
+	/**
+	 * retrait d'un stock
+	 * @param produit, stock
+	 */
+	public void retraitStrock(String produit, int stock) {
+		if (stock >= quantite) {
+            stock -= quantite;
+            System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
+        } else {
+            System.out.println("Stock insuffisant pour le produit : " + produit);
+        }
+	}
+	 
+    /**
+     * Gerer un stock
+     * @param typeOperation, produit, quantite, stock
+     */
     public void gererStock(String typeOperation, String produit, int quantite, int stock) {
-        // Ajout au stock
+        
         if (typeOperation.equals("ajout")) {
-            stock += quantite;
-            System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
-        // Retrait du stock
+            ajoutStock(produit, stock);
+            
         } else if (typeOperation.equals("retrait")) {
-            if (stock >= quantite) {
-                stock -= quantite;
-                System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
-            } else {
-                System.out.println("Stock insuffisant pour le produit : " + produit);
-            }
-        // Si l'action est inconnu
+            retraitStock(produit, stock);
+       
         } else {
             System.out.println("Opération inconnue.");
         }
